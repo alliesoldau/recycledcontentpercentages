d4af7e452f2fdac538cd4849028f6a965fe54dfc
alliesoldau <86414056+alliesoldau@users.noreply.github.com>
2022-05-12 14:10:43 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>PDF Form</title>
+</head>
+<body>
+
+    <div class="container mt-5">
+        <form>
+            <h1>Create your own PDF></h1>
+            <p> fill out below to download pdf</p>
+            <input type="text" name="fname" placeholder="First Name">
+            <input type="text" name="lname" placeholder="Last Name">
+            <input type="email" name="email" placeholder="Email">
+            <input type="tel" name="phone" placeholder="Phone">
+            <textarea name="message" placeholder="Your Message"></textarea>
+            <button type="submit">Create PDF</button>
+
+        </form>
+    </div>
+    
+</body>
+</html>
